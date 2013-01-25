3fcd10c95a8a4620b25cad2ffd39c377ea58c3c3
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2013-01-25 21:35:47 +1300

    Edit

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -3,22 +3,22 @@ require 'json'
 module MessageHandler
   def handle_websocket_message(messsage,websocket)
     data = JSON.parse(messsage)
-    handle_on_message(messsage) if data[:on]
-    handle_off_message(messsage) if data[:off]
-    handle_status_message(messsage) if data[:status]
+    handle_on_message(data) if data[:on]
+    handle_off_message(data) if data[:off]
+    handle_status_message(data) if data[:status]
   end
 
-  def handle_on_message(messsage)
-    turn_all_on if messsage[:on] == "all"
-    messsage[:on].each { |output| turn_on_output(output) }
+  def handle_on_data(data)
+    turn_all_on if data[:on] == "all"
+    data[:on].each { |output| turn_on_output(output) }
   end
 
-  def handle_off_message(messsage)
-    turn_all_on if messsage[:off] == "all"
-    messsage[:off].each { |output| turn_off_output(output) }  
+  def handle_off_message(data)
+    turn_all_on if data[:off] == "all"
+    data[:off].each { |output| turn_off_output(output) }  
   end
 
-  def handle_status_message(messsage)
+  def handle_status_message(data)
 
   end
 end
