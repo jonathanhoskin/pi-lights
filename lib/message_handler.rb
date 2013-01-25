8668264743e5321b811eff50ba5a52e71a8c4785
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2013-01-25 21:45:34 +1300

    Edit

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -1,29 +1,29 @@
 require 'json'
 
 module MessageHandler
-  def handle_websocket_message(messsage,websocket)
-    data = JSON.parse(messsage)
+  def handle_websocket_message(msg,ws)
+    data = JSON.parse(msg)
     puts "Data: #{data}"
-    handle_on_data(data) if data["on"]
-    handle_off_data(data) if data["off"]
-    handle_status_data(data) if data["status"]
+    handle_on_data(data,ws) if data["on"]
+    handle_off_data(data,ws) if data["off"]
+    handle_status_data(data,ws) if data["status"]
   end
 
-  def handle_on_data(data,websocket)
+  def handle_on_data(data,ws)
     puts "Data in handle ON: #{data}"
     turn_all_on if data["on"] == "all"
     data["on"].each { |output| turn_on_output(output) }
-    websocket.send "Turned On"
+    ws.send "Turned On"
   end
 
-  def handle_off_data(data,websocket)
+  def handle_off_data(data,ws)
     puts "Data in handle OFF: #{data}"
     turn_all_on if data["off"] == "all"
     data["off"].each { |output| turn_off_output(output) }
-    websocket.send "Turned Off"
+    ws.send "Turned Off"
   end
 
-  def handle_status_data(data,websocket)
-    websocket.send "Status"
+  def handle_status_data(data,ws)
+    ws.send "Status"
   end
 end
