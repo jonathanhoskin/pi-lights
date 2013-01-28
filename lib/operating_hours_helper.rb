09ce057fc11cfc3ed97ffd3881209b0ff65a675c
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2013-01-28 16:49:01 +1300

    Edit

--- lib/operating_hours_helper.rb
+++ lib/operating_hours_helper.rb
@@ -1,10 +1,30 @@
+require 'activesupport'
+
 module OperatingHoursHelper
 
   # LAT = -36.90194388138392
   # LNG = 174.62602709730263
 
   def inside_operating_hours
+    current_time = Time.now
+    next_sunset = Time.parse(`python sunset.py`.chomp)
+    next_sunrise = Time.parse(`python sunrise.py`.chomp)
+
+    # After sunset
+    return true if next_sunrise_is_tomorrow(next_sunrise,current_time) && next_sunset_is_tomorrow(next_sunset,current_time)
+
+    # Before sunrise
+    return true if !next_sunrise_is_tomorrow(next_sunrise,current_time)
+    
     false
+  end
+
+  def next_sunrise_is_tomorrow(next_sunrise,current_time)
+    return next_sunrise.day > current_time.day
+  end
+
+  def next_sunset_is_tomorrow(next_sunset,current_time)
+    return next_sunset.day > current_time.day
   end
 
 end
---
+++ sunset.py
@@ -0,0 +1,18 @@
+import ephem
+from datetime import datetime
+from dateutil import tz
+
+obs = ephem.Observer()
+obs.lat = '-36.9'
+obs.long= '174.6'
+obs.date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
+
+sun = ephem.Sun()
+
+utc = obs.next_setting(sun).datetime()
+utc = utc.replace(tzinfo=from_zone)
+
+print utc.astimezone(to_zone)
