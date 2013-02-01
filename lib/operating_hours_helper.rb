5b368b0792648afc00771438260d2626217c0603
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2013-02-01 20:01:14 +1300

    Edit

--- lib/operating_hours_helper.rb
+++ lib/operating_hours_helper.rb
@@ -7,21 +7,35 @@ module OperatingHoursHelper
     next_sunset = Time.parse(`python sunset.py`.chomp)
     next_sunrise = Time.parse(`python sunrise.py`.chomp)
 
-    # After sunset, when sunrise and sunset are tomorrow
-    return true if next_sunrise_is_tomorrow(next_sunrise,current_time) && next_sunset_is_tomorrow(next_sunset,current_time)
+    puts "Checking operating hours"
 
-    # Before sunrise
-    return true if !next_sunrise_is_tomorrow(next_sunrise,current_time)
+    # After sunset, when sunrise and sunset are tomorrow
+    if next_sunrise_is_tomorrow(next_sunrise,current_time) && next_sunset_is_tomorrow(next_sunset,current_time)
+      puts "After sunset, when sunrise and sunset are tomorrow"
+      return true 
+    end
+
+    # Before sunrise, when sunrise is today
+    if !next_sunrise_is_tomorrow(next_sunrise,current_time)
+      puts "Before sunrise, when sunrise is today"
+      return true
+    end
 
     # After sunset, when sunset is earlier today, sunrise is tomorrow
-    return true if next_sunrise_is_tomorrow(next_sunrise,current_time) && next_sunset < current_time
+    if next_sunrise_is_tomorrow(next_sunrise,current_time) && next_sunset < current_time
+      puts "After sunset, when sunset is earlier today, sunrise is tomorrow"
+      return true
+    end
 
+    puts "Not inside operating hours"
     false
   end
 
   def end_of_today
     now = Time.now
-    return Time.local(now.year, now.month, now.day, 23, 59, 59, 999999.999)
+    end_of_today = Time.local(now.year, now.month, now.day, 23, 59, 59, 999999.999)
+    puts "End of today: #{end_of_today}"
+    return end_of_today
   end
 
   def next_sunrise_is_tomorrow(next_sunrise,current_time)
